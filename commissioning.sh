5bb2603e91446740a1cc7f81bc1c08401bb801e6
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-08-23 14:12:55 +0530

    Update commissioning.sh

--- commissioning.sh
+++ commissioning.sh
@@ -1,3 +1,4 @@
+cd /home/sreenath/Documents/git/Sreenath-Commission
 g++ commissioning-server.cpp -lpqxx -lpq -std=c++14 -o commissioning-server -static-libstdc++ -ljsoncpp -lcrypt
 sudo service commissioning-server stop
 sudo cp commissioning-server /bin/commissioning-server
