60cee1ad06bd8ab7a49558ae9b4aaa68d8b9e37d
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-08-24 17:10:10 +0530

    Update commissioning.sh

--- commissioning.sh
+++ commissioning.sh
@@ -12,7 +12,7 @@ status=$(sudo service commissioning-server status)
 #echo $status
 sleep 1
 #status=$(./clientNew 127.0.0.1 5433 "0000000020[{\"username\": \"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"showUTCtimeAndLocaltime\"}]")
-status=$(sudo ./clientNew 127.0.0.1 5433 0000000020"[{\"username\": \"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"PlcDataloginStatus\",\"UTCTime\":\"2022-06-27 06:28:40\"}]")
+status=$(./clientNew 127.0.0.1 5433 0000000020"[{\"username\": \"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"PlcDataloginStatus\",\"UTCTime\":\"2022-06-27 06:28:40\"}]")
 #echo $status
 echo $status > result.json
 read contains < <(jq .field[0].result result.json)
