2e25f56315bb38d54f03963f2b52c11b84d41a90
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-08-24 17:45:42 +0530

    Update commissioning.sh

--- commissioning.sh
+++ commissioning.sh
@@ -4,10 +4,22 @@ sudo apt-get install libpqxx-dev
 sudo apt-get install libjsoncpp-dev
 sudo apt-get install libgcrypt-dev
 g++ commissioning-server.cpp -lpqxx -lpq -std=c++14 -o commissioning-server -static-libstdc++ -ljsoncpp -lcrypt
-sudo service commissioning-server stop
-sudo cp commissioning-server /bin/commissioning-server
-sudo systemctl daemon-reload
-sudo systemctl start commissioning-server.service
+echo "Step 11/11 Create, Enable and Start the commissioning-server Service"
+    sudo firewall-cmd --zone=public --permanent --add-port=5433/tcp
+    sudo firewall-cmd --reload
+    if [ ! -d /bin/commissioning-server ]; then
+    	sudo mkdir /bin/commissioning-server
+    fi
+	sudo cp commissioning-server /bin/commissioning-server
+	status=$?
+	if [ $status -ne 0 ];then
+		echo "Installation failed at step11"
+		exit
+	fi
+	#sudo cp commissioning-server.service /etc/systemd/system
+	sudo systemctl daemon-reload
+	sudo systemctl enable $currpath/commissioning-server.service
+	sudo systemctl start commissioning-server.service
 status=$(sudo service commissioning-server status)
 #echo $status
 sleep 1
