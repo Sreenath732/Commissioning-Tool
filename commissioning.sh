503a41c3d08dfaa44e503cefd5e41a0b61ddfad0
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-08-24 15:49:52 +0530

    Update commissioning.sh

--- commissioning.sh
+++ commissioning.sh
@@ -1,4 +1,6 @@
 cd /home/sreenath/Documents/git/Sreenath-Commission
+sudo apt-get install libpqxx-4.0v5
+sudo apt-get install libpqxx-dev
 g++ commissioning-server.cpp -lpqxx -lpq -std=c++14 -o commissioning-server -static-libstdc++ -ljsoncpp -lcrypt
 sudo service commissioning-server stop
 sudo cp commissioning-server /bin/commissioning-server
