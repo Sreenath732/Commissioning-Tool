075d0cd88a90a11e68fb158dc8ebb8749a45cc80
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-08-24 16:10:26 +0530

    Update commissioning.sh

--- commissioning.sh
+++ commissioning.sh
@@ -4,7 +4,7 @@ sudo apt-get install libpqxx-dev
 sudo apt-get install libjsoncpp-dev
 sudo apt-get install libgcrypt-dev
 g++ commissioning-server.cpp -lpqxx -lpq -std=c++14 -o commissioning-server -static-libstdc++ -ljsoncpp -lcrypt
-sudo service commissioning-server stop
+#sudo service commissioning-server stop
 sudo cp commissioning-server /bin/commissioning-server
 sudo systemctl daemon-reload
 sudo systemctl start commissioning-server.service
