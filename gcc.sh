29c38e0cf617e3d77915ef02be28812136afb122
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-08-24 16:03:56 +0530

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -1,4 +1,6 @@
 cd /home/sreenath/Documents/git/Sreenath-Commission
+sudo apt-get install libpqxx-4.0v5
+sudo apt-get install libpqxx-dev
+sudo apt-get install libjsoncpp-dev
+sudo apt-get install libgcrypt-dev
 g++ commissioning-server.cpp -lpqxx -lpq -std=c++14 -o commissioning-server -static-libstdc++ -ljsoncpp -lcrypt
-
-
