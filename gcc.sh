b7e235031eeeebf99d77f0fddc33f2f9100ee428
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-08-24 16:20:27 +0530

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -3,4 +3,6 @@ sudo apt-get install libpqxx-4.0v5
 sudo apt-get install libpqxx-dev
 sudo apt-get install libjsoncpp-dev
 sudo apt-get install libgcrypt-dev
-g++ commissioning-server.cpp -lpqxx -lpq -std=c++14 -o commissioning-server -static-libstdc++ -ljsoncpp -lcrypt
+sree=$(g++ commissioning-server.cpp -lpqxx -lpq -std=c++14 -o commissioning-server -static-libstdc++ -ljsoncpp -lcrypt)
+echo $sree
+
