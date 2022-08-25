3cac117bbf5e38d0802d8a3a2f48389931411f94
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-08-25 11:22:55 +0530

    Update commissioning.sh

--- commissioning.sh
+++ commissioning.sh
@@ -9,6 +9,10 @@ sudo cp commissioning-server.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable commissioning-server.service
 sudo systemctl start commissioning-server.service
+cd /bin/commissioning-server
+ls
+cd /etc/systemd/system
+ls
 sudo service commissioning-server status
 sleep 1
 #status=$(./clientNew 127.0.0.1 5433 "0000000020[{\"username\": \"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"showUTCtimeAndLocaltime\"}]")
