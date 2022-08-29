614858dc402c0311fe01c406bf579975e6870c09
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-08-29 14:45:34 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 sudo apt-get install gnome-terminal
+cd project
+sudo chmod 777 *
 gnome-terminal -- ./commissioning-server 2020
 
 #status=$(./clientNew 127.0.0.1 5433 "0000000020[{\"username\": \"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"showUTCtimeAndLocaltime\"}]")
