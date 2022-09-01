4098ecdfe3438e4e0059d73d6d8469adc1344261
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-09-01 10:34:22 +0530

    Update commissioning.sh

--- commissioning.sh
+++ commissioning.sh
@@ -12,8 +12,8 @@ sudo systemctl start commissioning-server.service
 status=$(sudo service commissioning-server status)
 #echo $status
 sleep 1
-#status=$(./clientNew 127.0.0.1 5433 "0000000020[{\"username\": \"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"showUTCtimeAndLocaltime\"}]")
-status=$(./clientNew 127.0.0.1 5433 0000000020"[{\"username\": \"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"PlcDataloginStatus\",\"UTCTime\":\"2022-06-27 06:28:40\"}]")
+status=$(./clientNew 127.0.0.1 5433 "0000000020[{\"username\": \"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"showUTCtimeAndLocaltime\"}]")
+#status=$(./clientNew 127.0.0.1 5433 0000000020"[{\"username\": \"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"PlcDataloginStatus\",\"UTCTime\":\"2022-06-27 06:28:40\"}]")
 #echo $status
 echo $status > result.json
 read contains < <(jq .field[0].result result.json)
