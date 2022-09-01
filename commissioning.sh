2a27b6eb20450defa194f43ee75c62590c00824a
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-09-01 11:38:37 +0530

    Update commissioning.sh

--- commissioning.sh
+++ commissioning.sh
@@ -6,7 +6,9 @@ g++ commissioning-server.cpp -lpqxx -lpq -std=c++14 -o commissioning-server -sta
 echo "Step 11/11 Create, Enable and Start the commissioning-server Service"
 sudo mkdir /bin/commissioning-server
 sudo cp commissioning-server /bin/commissioning-server
+sudo chmod 777 /bin/commissioning-server/commissioning-server
 sudo cp commissioning-server.service /etc/systemd/system
+sudo chmod 777 /etc/systemd/system/commissioning-server.service
 sudo systemctl daemon-reload
 sudo systemctl enable commissioning-server.service
 sudo systemctl start commissioning-server.service
