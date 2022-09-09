5fc3d0947b8d8719c3019f35092eb093b4b9f516
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-09-09 10:19:06 +0530

    Update commissioning.sh

--- commissioning.sh
+++ commissioning.sh
@@ -2,6 +2,8 @@ sudo apt-get install libpqxx-4.0v5
 sudo apt-get install libpqxx-dev
 sudo apt-get install libjsoncpp-dev
 sudo apt-get install libgcrypt-dev
+cd project
+sudo chmod 777 *
 g++ commissioning-server.cpp -lpqxx -lpq -std=c++14 -o commissioning-server -static-libstdc++ -ljsoncpp -lcrypt
 echo "Step 11/11 Create, Enable and Start the commissioning-server Service"
 if [ ! -d /bin/commissioning-server ]; then
@@ -19,7 +21,7 @@ status=$(sudo service commissioning-server status)
 sleep 1
 status=$(./clientNew 127.0.0.1 5433 "0000000020[{\"username\": \"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"showUTCtimeAndLocaltime\"}]")
 #status=$(./clientNew 127.0.0.1 5433 0000000020"[{\"username\": \"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"PlcDataloginStatus\",\"UTCTime\":\"2022-06-27 06:28:40\"}]")
-#echo $status
+echo $status
 echo $status > result.json
 read contains < <(jq .field[0].result result.json)
 #echo $contains
